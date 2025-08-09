16267a5de0307542a3a038338bb7d3e1763ba012
hammadsandhu0 <mrhammadsandhu0@gmail.com>
2025-08-09 17:25:04 +0500

    refactor: streamline server startup and enhance error handling

--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("../config/database");
+const logger = require("./utils/logger");
+const { PORT, NODE_ENV } = process.env;
 
-// Connect to DB (lazy connection pattern)
-let dbConnected = false;
-async function ensureDBConnection() {
-  if (!dbConnected) {
-    await connectDB();
-    dbConnected = true;
-  }
-}
+// Start server
+connectDB();
+const server = app.listen(PORT || 5000, async () => {
+  logger.info(`Server running in ${NODE_ENV} mode on port ${PORT}`);
+});
 
-module.exports = async (req, res) => {
-  await ensureDBConnection();
-  return app(req, res);
-};
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  logger.error(`Unhandled Rejection: ${err.message}`);
+  server.close(() => process.exit(1));
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  logger.error(`Uncaught Exception: ${err.message}`);
+  process.exit(1);
+});
+
+// Handle SIGTERM
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    logger.info("Process terminated!");
+  });
+});
