e10af88e0423fcf7f8a529d3b65789e3967bae03
hammadsandhu0 <mrhammadsandhu0@gmail.com>
2025-09-03 22:36:59 +0500

    refactor: enhance getAllProducts and getProductsByCategory to support additional filters and sorting options

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -20,50 +20,201 @@ const { generateUniqueSlug, createSlug } = require("../utils/slug");
 const formatAdditionalInfo = require("../utils/formatAdditionalInfo");
 
 // ------------------ GET ALL PRODUCTS ------------------
+// exports.getAllProducts = catchAsync(async (req, res, next) => {
+//   const filter = {};
+//   const andConditions = [];
+
+//   /* ------------------ FILTERING ------------------ */
+//   if (req.query.category) {
+//     const category = await Category.findOne({
+//       slug: req.query.category,
+//     }).select("_id");
+//     if (!category)
+//       return errorResponse(res, "No category found with that slug", 404);
+//     andConditions.push({ category: category._id });
+//   }
+
+//   if (req.query.subCategory)
+//     andConditions.push({ subCategory: req.query.subCategory });
+
+//   if (req.query.minPrice || req.query.maxPrice) {
+//     const priceFilter = {};
+//     if (req.query.minPrice) priceFilter.$gte = Number(req.query.minPrice);
+//     if (req.query.maxPrice) priceFilter.$lte = Number(req.query.maxPrice);
+//     andConditions.push({ price: priceFilter });
+//   }
+
+//   if (req.query.minRating) {
+//     andConditions.push({
+//       ratingsAverage: { $gte: Number(req.query.minRating) },
+//     });
+//   }
+
+//   if (req.query.tag) {
+//     const tag = await Tag.findOne({ slug: req.query.tag }).select("_id");
+//     if (!tag) return errorResponse(res, "No tag found with that slug", 404);
+//     andConditions.push({ tags: tag._id });
+//   }
+
+//   if (req.query.search) {
+//     const searchRegex = { $regex: req.query.search, $options: "i" };
+//     andConditions.push({
+//       $or: [{ name: searchRegex }, { description: searchRegex }],
+//     });
+//   }
+
+//   if (andConditions.length > 0) filter.$and = andConditions;
+
+//   /* ------------------ QUERY WITH POPULATE ------------------ */
+//   let query = Product.find(filter)
+//     .populate("tags", "name slug")
+//     .populate("category", "name slug")
+//     .populate("subCategory", "name slug")
+//     .populate({
+//       path: "variations",
+//       populate: {
+//         path: "attribute",
+//         model: "Attribute",
+//         select: "slug name type values",
+//       },
+//     })
+//     .populate({
+//       path: "variation_options",
+//       populate: {
+//         path: "attributes.attribute",
+//         model: "Attribute",
+//         select: "slug name type values",
+//       },
+//     })
+//     .populate("image", "original thumbnail")
+//     .populate("gallery", "original thumbnail");
+
+//   const features = new APIFeatures(query, req.query)
+//     .sort()
+//     .limitFields()
+//     .paginate();
+
+//   let products = await features.query;
+
+//   /* ------------------ FORMAT ADDITIONAL INFO ------------------ */
+//   products = products.map((product) => {
+//     const obj = product.toObject();
+//     obj.additional_info = formatAdditionalInfo(obj);
+//     return obj;
+//   });
+
+//   return successResponse(
+//     res,
+//     { products, results: products.length },
+//     "Products fetched successfully"
+//   );
+// });
 exports.getAllProducts = catchAsync(async (req, res, next) => {
   const filter = {};
   const andConditions = [];
 
-  /* ------------------ FILTERING ------------------ */
-  if (req.query.category) {
-    const category = await Category.findOne({
-      slug: req.query.category,
-    }).select("_id");
-    if (!category)
+  const {
+    category,
+    subCategory,
+    minPrice,
+    maxPrice,
+    minRating,
+    tag,
+    tags,
+    attributes,
+    search,
+    inStock,
+    onSale,
+    sort_by,
+  } = req.query;
+
+  /* ------------------ CATEGORY FILTER ------------------ */
+  if (category) {
+    const categoryDoc = await Category.findOne({ slug: category }).select(
+      "_id"
+    );
+    if (!categoryDoc)
       return errorResponse(res, "No category found with that slug", 404);
-    andConditions.push({ category: category._id });
+    andConditions.push({ category: categoryDoc._id });
   }
 
-  if (req.query.subCategory)
-    andConditions.push({ subCategory: req.query.subCategory });
+  if (subCategory) {
+    andConditions.push({ subCategory });
+  }
 
-  if (req.query.minPrice || req.query.maxPrice) {
+  /* ------------------ PRICE FILTER ------------------ */
+  if (minPrice || maxPrice) {
     const priceFilter = {};
-    if (req.query.minPrice) priceFilter.$gte = Number(req.query.minPrice);
-    if (req.query.maxPrice) priceFilter.$lte = Number(req.query.maxPrice);
+    if (minPrice) priceFilter.$gte = Number(minPrice);
+    if (maxPrice) priceFilter.$lte = Number(maxPrice);
     andConditions.push({ price: priceFilter });
   }
 
-  if (req.query.minRating) {
-    andConditions.push({
-      ratingsAverage: { $gte: Number(req.query.minRating) },
-    });
+  /* ------------------ RATING FILTER ------------------ */
+  if (minRating) {
+    andConditions.push({ ratingsAverage: { $gte: Number(minRating) } });
   }
 
-  if (req.query.tag) {
-    const tag = await Tag.findOne({ slug: req.query.tag }).select("_id");
-    if (!tag) return errorResponse(res, "No tag found with that slug", 404);
-    andConditions.push({ tags: tag._id });
+  /* ------------------ SINGLE TAG FILTER ------------------ */
+  if (tag) {
+    const tagDoc = await Tag.findOne({ slug: tag }).select("_id");
+    if (!tagDoc) return errorResponse(res, "No tag found with that slug", 404);
+    andConditions.push({ tags: tagDoc._id });
   }
 
-  if (req.query.search) {
-    const searchRegex = { $regex: req.query.search, $options: "i" };
+  /* ------------------ MULTIPLE TAGS FILTER ------------------ */
+  if (tags) {
+    const tagSlugs = tags.split(",");
+    const tagDocs = await Tag.find({ slug: { $in: tagSlugs } }).select("_id");
+    andConditions.push({ tags: { $in: tagDocs.map((t) => t._id) } });
+  }
+
+  /* ------------------ STOCK FILTER ------------------ */
+  if (inStock === "true") {
+    andConditions.push({ in_stock: true });
+  }
+
+  /* ------------------ ON SALE FILTER ------------------ */
+  if (onSale === "true") {
+    andConditions.push({ on_sale: true });
+  }
+
+  /* ------------------ ATTRIBUTES FILTER ------------------ */
+  if (attributes) {
+    const attrArray = attributes.split(",");
+    const attrConditions = [];
+    for (const attr of attrArray) {
+      const [attrName, value] = attr.split(":");
+      const attribute = await Attribute.findOne({ slug: attrName });
+      if (attribute) {
+        attrConditions.push({
+          "variation_options.attributes": {
+            $elemMatch: { attribute: attribute._id, value },
+          },
+        });
+      }
+    }
+    if (attrConditions.length > 0) andConditions.push(...attrConditions);
+  }
+
+  /* ------------------ SEARCH FILTER ------------------ */
+  if (search) {
+    const searchRegex = { $regex: search, $options: "i" };
     andConditions.push({
       $or: [{ name: searchRegex }, { description: searchRegex }],
     });
   }
 
+  /* ------------------ APPLY AND CONDITIONS ------------------ */
   if (andConditions.length > 0) filter.$and = andConditions;
+
+  /* ------------------ SORT OPTIONS ------------------ */
+  const SORT_OPTIONS = {
+    "new-arrival": { createdAt: -1 },
+    "best-selling": { ratingsQuantity: -1 },
+    lowest: { price: 1 },
+    highest: { price: -1 },
+  };
 
   /* ------------------ QUERY WITH POPULATE ------------------ */
   let query = Product.find(filter)
@@ -89,11 +240,16 @@ exports.getAllProducts = catchAsync(async (req, res, next) => {
     .populate("image", "original thumbnail")
     .populate("gallery", "original thumbnail");
 
-  const features = new APIFeatures(query, req.query)
-    .sort()
-    .limitFields()
-    .paginate();
+  const features = new APIFeatures(query, req.query).limitFields().paginate();
 
+  // Apply sorting dynamically
+  if (sort_by && SORT_OPTIONS[sort_by]) {
+    features.query = features.query.sort(SORT_OPTIONS[sort_by]);
+  } else {
+    features.query = features.query.sort({ createdAt: -1 }); // default sort
+  }
+
+  /* ------------------ EXECUTE QUERY ------------------ */
   let products = await features.query;
 
   /* ------------------ FORMAT ADDITIONAL INFO ------------------ */
@@ -544,6 +700,138 @@ exports.deleteProduct = catchAsync(async (req, res, next) => {
 });
 
 // ------------------ GET PRODUCTS BY CATEGORY (WITH FILTERS) ------------------
+// exports.getProductsByCategory = catchAsync(async (req, res, next) => {
+//   const { parent: categorySlug, child: subCategorySlug } = req.query;
+//   if (!categorySlug) {
+//     return errorResponse(res, "Parent category slug is required", 400);
+//   }
+
+//   // Find parent category
+//   const category = await Category.findOne({ slug: categorySlug });
+//   if (!category) {
+//     return errorResponse(res, "No category found with that slug", 404);
+//   }
+
+//   // Basic filter setup
+//   let filter = { category: category._id };
+
+//   // Sub-category filter
+//   if (subCategorySlug) {
+//     const subCategory = category.children.find(
+//       (child) => child.slug === subCategorySlug
+//     );
+//     if (!subCategory) {
+//       return errorResponse(res, "No sub-category found with that slug", 404);
+//     }
+//     filter.subCategory = subCategory._id;
+//   }
+
+//   // ------------------ ADD FILTERS ------------------
+//   const { minPrice, maxPrice, inStock, onSale, attributes, tags, search } =
+//     req.query;
+
+//   // Price Range Filter
+//   if (minPrice || maxPrice) {
+//     filter.price = {};
+//     if (minPrice) filter.price.$gte = Number(minPrice);
+//     if (maxPrice) filter.price.$lte = Number(maxPrice);
+//   }
+
+//   // Stock Filter
+//   if (inStock === "true") filter.in_stock = true;
+
+//   // On Sale Filter
+//   if (onSale === "true") filter.on_sale = true;
+
+//   // Tags Filter (comma separated slugs)
+//   if (tags) {
+//     const tagSlugs = tags.split(",");
+//     const tagDocs = await Tag.find({ slug: { $in: tagSlugs } }).select("_id");
+//     filter.tags = { $in: tagDocs.map((tag) => tag._id) };
+//   }
+
+//   // Attribute Filters (Example: ?attributes=color:red,size:XL)
+//   if (attributes) {
+//     const attrArray = attributes.split(",");
+//     const attrConditions = [];
+
+//     for (const attr of attrArray) {
+//       const [attrName, value] = attr.split(":");
+//       const attribute = await Attribute.findOne({ slug: attrName });
+//       if (attribute) {
+//         attrConditions.push({
+//           "variation_options.attributes": {
+//             $elemMatch: { attribute: attribute._id, value },
+//           },
+//         });
+//       }
+//     }
+
+//     if (attrConditions.length > 0) {
+//       filter.$and = attrConditions;
+//     }
+//   }
+
+//   // Search Filter (on product name or description)
+//   if (search) {
+//     filter.$or = [
+//       { name: { $regex: search, $options: "i" } },
+//       { description: { $regex: search, $options: "i" } },
+//     ];
+//   }
+
+//   // ------------------ QUERY ------------------
+//   let query = Product.find(filter)
+//     .populate("tags", "name slug")
+//     .populate("category", "name slug")
+//     .populate("subCategory", "name slug")
+//     .populate({
+//       path: "variations",
+//       populate: {
+//         path: "attribute",
+//         model: "Attribute",
+//         select: "slug name type values",
+//       },
+//     })
+//     .populate({
+//       path: "variation_options",
+//       populate: {
+//         path: "attributes.attribute",
+//         model: "Attribute",
+//         select: "slug name type values",
+//       },
+//     })
+//     .populate("image", "original thumbnail")
+//     .populate("gallery", "original thumbnail")
+//     .populate({
+//       path: "reviews",
+//       populate: {
+//         path: "user",
+//         select: "name",
+//       },
+//       match: { is_approved: true },
+//     });
+
+//   const features = new APIFeatures(query, req.query)
+//     .sort()
+//     .limitFields()
+//     .paginate();
+
+//   let products = await features.query;
+
+//   products = products.map((product) => {
+//     const obj = product.toObject();
+//     obj.additional_info = formatAdditionalInfo(obj);
+//     return obj;
+//   });
+
+//   return successResponse(
+//     res,
+//     { products, results: products.length },
+//     "Products fetched successfully"
+//   );
+// });
+
 exports.getProductsByCategory = catchAsync(async (req, res, next) => {
   const { parent: categorySlug, child: subCategorySlug } = req.query;
   if (!categorySlug) {
@@ -556,7 +844,7 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
     return errorResponse(res, "No category found with that slug", 404);
   }
 
-  // Basic filter setup
+  // Base filter
   let filter = { category: category._id };
 
   // Sub-category filter
@@ -570,35 +858,42 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
     filter.subCategory = subCategory._id;
   }
 
-  // ------------------ ADD FILTERS ------------------
-  const { minPrice, maxPrice, inStock, onSale, attributes, tags, search } =
-    req.query;
+  // Extract query params
+  const {
+    minPrice,
+    maxPrice,
+    inStock,
+    onSale,
+    attributes,
+    tags,
+    search,
+    sort_by,
+  } = req.query;
 
-  // Price Range Filter
+  // ---------------- PRICE FILTER ----------------
   if (minPrice || maxPrice) {
     filter.price = {};
     if (minPrice) filter.price.$gte = Number(minPrice);
     if (maxPrice) filter.price.$lte = Number(maxPrice);
   }
 
-  // Stock Filter
+  // ---------------- STOCK FILTER ----------------
   if (inStock === "true") filter.in_stock = true;
 
-  // On Sale Filter
+  // ---------------- ON SALE FILTER ----------------
   if (onSale === "true") filter.on_sale = true;
 
-  // Tags Filter (comma separated slugs)
+  // ---------------- TAGS FILTER ----------------
   if (tags) {
     const tagSlugs = tags.split(",");
     const tagDocs = await Tag.find({ slug: { $in: tagSlugs } }).select("_id");
     filter.tags = { $in: tagDocs.map((tag) => tag._id) };
   }
 
-  // Attribute Filters (Example: ?attributes=color:red,size:XL)
+  // ---------------- ATTRIBUTES FILTER ----------------
   if (attributes) {
     const attrArray = attributes.split(",");
     const attrConditions = [];
-
     for (const attr of attrArray) {
       const [attrName, value] = attr.split(":");
       const attribute = await Attribute.findOne({ slug: attrName });
@@ -610,13 +905,10 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
         });
       }
     }
-
-    if (attrConditions.length > 0) {
-      filter.$and = attrConditions;
-    }
+    if (attrConditions.length > 0) filter.$and = attrConditions;
   }
 
-  // Search Filter (on product name or description)
+  // ---------------- SEARCH FILTER ----------------
   if (search) {
     filter.$or = [
       { name: { $regex: search, $options: "i" } },
@@ -624,7 +916,15 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
     ];
   }
 
-  // ------------------ QUERY ------------------
+  // ---------------- SORT OPTIONS ----------------
+  const SORT_OPTIONS = {
+    "new-arrival": { createdAt: -1 }, // Latest first
+    "best-selling": { ratingsQuantity: -1 }, // Proxy for best-selling
+    lowest: { price: 1 }, // Ascending price
+    highest: { price: -1 }, // Descending price
+  };
+
+  // ---------------- QUERY ----------------
   let query = Product.find(filter)
     .populate("tags", "name slug")
     .populate("category", "name slug")
@@ -656,13 +956,19 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
       match: { is_approved: true },
     });
 
-  const features = new APIFeatures(query, req.query)
-    .sort()
-    .limitFields()
-    .paginate();
+  const features = new APIFeatures(query, req.query).limitFields().paginate();
 
+  // Apply sorting
+  if (sort_by && SORT_OPTIONS[sort_by]) {
+    features.query = features.query.sort(SORT_OPTIONS[sort_by]);
+  } else {
+    features.query = features.query.sort({ createdAt: -1 }); // default
+  }
+
+  // Execute query
   let products = await features.query;
 
+  // Format additional info
   products = products.map((product) => {
     const obj = product.toObject();
     obj.additional_info = formatAdditionalInfo(obj);
@@ -675,6 +981,7 @@ exports.getProductsByCategory = catchAsync(async (req, res, next) => {
     "Products fetched successfully"
   );
 });
+
 
 
 
